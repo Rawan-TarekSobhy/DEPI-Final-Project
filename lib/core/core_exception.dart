9ac918e336cadc378993244935adc4a88a14eb08
Amr530 <166232199+Amr530@users.noreply.github.com>
2025-12-01 23:32:53 +0200

    Remove Android platform and related assets

--- lib/core/core_exception.dart
+++
@@ -1,9 +0,0 @@
-class InvalidCredentialsException implements Exception {
-  String? message;
-  InvalidCredentialsException() : message = 'Invalid login credentials';
-}
-
-class EmailAlreadyExistsException implements Exception {
-  String? message;
-  EmailAlreadyExistsException() : message = 'Email is already registered';
-}
